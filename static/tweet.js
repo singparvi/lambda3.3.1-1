7371ff688e2b181acf23dffdd25e7fd1ea587900
John Hurdle <john.renken@lambdaschool.com>
2021-04-13 17:16:32 -0600

    replace textareas with input tags and tweak jquery

--- static/tweet.js
+++ static/tweet.js
@@ -1,11 +1,9 @@
-$('#author_text_input').innerText = "Please enter a twitter user's handle";
 $('#author_submit').click(function () {
     $.ajax({
-        url: '/add_user?twitter_handle=' + $('#author_text_input').val(),
+        url: '/add_user?twitter_handle=' + $('#author_name_input').val(),
         type: 'GET',
     });
 });
-$('#tweet_text_input').innerText = "Please enter a tweet to classify";
 $('#tweet_submit').click(function () {
     $.ajax({
         url: '/predict_author?tweet_to_classify=' + $('#tweet_text_input').val(),
